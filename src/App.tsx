30f1cb0c27a2ca39ab9d13f71fce2ced414e2ef7
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2025-01-25 23:08:53 +0100

    FIRST COMMIT

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import Page from "@/app/dashboard/page.tsx";
+
+export default function App() {
+  return (
+      <Page />
+  )
+}
